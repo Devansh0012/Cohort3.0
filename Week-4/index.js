cb15a53550106f4fe62bafeb5cdb8ef42dd5f812
Devansh0012 <devanshdubey0012@gmail.com>
2024-12-24 14:47:08 +0530

    added week 4

---
+++ Week-4/index.js
@@ -0,0 +1,9 @@
+// const chalk = require("chalk");
+
+// console.log(chalk.colorNames);
+
+import chalk from "chalk";
+
+console.log(chalk.blue("hello"));
+console.log(chalk.red.bold("error"));
+console.log(chalk.green.underline("success"));
